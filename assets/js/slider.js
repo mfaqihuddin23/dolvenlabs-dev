d77378b6ac0bf38537fa295b348c70e52208f352
Makruf Al Hafiz <makrufalhafiz@MacBook-Pro.local>
2022-09-08 13:26:20 +0700

    invest section

---
+++ assets/js/slider.js
@@ -0,0 +1,45 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const swiper1 = new Swiper('.swiper1', {
+  slidesPerView: 'auto',
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
