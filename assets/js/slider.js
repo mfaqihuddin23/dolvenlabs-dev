e0733b2a48b334aff749a9d5b7c43ede27adfdb3
Makruf Al Hafiz <makrufalhafiz@MacBook-Pro.local>
2022-09-08 15:29:57 +0700

    our team

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -43,3 +43,51 @@ const swiper1 = new Swiper('.swiper1', {
     el: '.swiper-scrollbar',
   },
 });
+
+const swiper2 = new Swiper('.swiper2', {
+  slidesPerView: 'auto',
+  spaceBetween: 70,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+  slidesPerView: 'auto',
+  spaceBetween: 70,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
